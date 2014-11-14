8bcbc670e0bddfe30636b8336185bdb096b7a397
Czarina Lou <czarinamarav@gmai.com>
2014-11-14 11:45:20 +0800

    add Preprocessing programs/codes

---
+++ Summarizer/PreProcessing/POS.py
@@ -0,0 +1,7 @@
+import nltk
+from nltk.tokenize import wordpunct_tokenize
+from nltk.tag import pos_tag
+def POS(str):
+   
+    
+    return nltk.pos_tag(wordpunct_tokenize(str))
---
+++ Summarizer/PreProcessing/tokenizer.py
@@ -0,0 +1,7 @@
+import nltk
+from nltk.tokenize import wordpunct_tokenize
+
+def tokenizer(str):
+    #wordpunct_tokenize
+    
+    return wordpunct_tokenize(str);
