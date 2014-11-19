a1a449dd398cc34f7f7c59fc427ad3543752653c
ellej16 <pjinxed.aranzaellej@gmail.com>
2014-11-19 10:20:34 +0800

    delete pos.py and tokenizer.py for merge into preprocessing.py

--- Summarizer/PreProcessing/POS.py
+++
@@ -1,8 +0,0 @@
-#POS tagger
-import nltk
-from nltk.tokenize import wordpunct_tokenize
-from nltk.tag import pos_tag
-def POS(str):
-   
-    
-    return nltk.pos_tag(wordpunct_tokenize(str))
--- Summarizer/PreProcessing/tokenizer.py
+++
@@ -1,8 +0,0 @@
-#tokenizer
-import nltk
-from nltk.tokenize import wordpunct_tokenize
-
-def tokenizer(str):
-    
-    
-    return wordpunct_tokenize(str);
