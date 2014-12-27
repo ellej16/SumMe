2fd9ae02c7e7a3924524cb44b2ad75881346ffaf
giodude29 <evangelistarenzgio@gail.com>
2014-12-27 03:07:18 -0800

    Edited Sample.py

--- NLG/Sample.py
+++ NLG/Sample.py
@@ -11,9 +11,14 @@ def setUp():
         s1 = Clause()
         np_woman = s1.add_subject(lex.getWord("woman"))
         np_woman.add_determiner(lex.getWord("the"))
-        vp_kiss = s1.add_verb(lex.getWord("kiss", "VERB"))
         
-        np_man = vp_kiss.add_object(lex.getWord("man"))
+		vp_kiss = s1.add_verb(lex.getWord("kiss", "VERB"))
+		np_the_curtain = NounPhrase(lex.getWord("curtain", "NOUN"), determiner=lex.getWord("the"))
+        pp_the_curtain = PrepositionalPhrase(lex.getWord("behind", "PREPOSITION"), [np_the_curtain])
+        
+        vp_kiss.add_prepositional_phrase(pp_the_curtain)
+		
+		np_man = vp_kiss.add_object(lex.getWord("man"))
         np_man.add_determiner(lex.getWord("the"))
         
         s1.set_verb_tense("past")       
