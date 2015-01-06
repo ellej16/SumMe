5f90d0df593da7e4db3c59b9f993c7339ee5aa3c
ellej16 <pjinxed.aranzaellej@gmail.com>
2015-01-06 08:07:00 +0800

    add Summarizer module to WebApp

---
+++ WebApp/summe_project/summe_app/Summarizer.py
@@ -0,0 +1,82 @@
+'author pol'
+from summe_app.PreProcessing import preprocessor
+
+#should contain these:
+#a list containing:
+	#sentence number,
+	#sentence itself
+	#words(this would be a dict)
+		#word
+		#POS tag
+article =  [] #is this shit even needed
+
+global sentences
+sentences = [] # sentence number, the sentence, the tuples of words 
+			#and their corresponding POS tags, and the language id
+			#when chunkSents is invoked chunks of the sentence is appended
+
+
+def chunkSents():
+	global sentences
+	for sents in sentences:
+		if sents[3] =="en":
+			sents.append(preprocessor.enChunk(sents[2]))
+			sentences[sents[0]] = sents
+		elif sents[3] =="tl":
+			sents.append(preprocessor.tlChunk(sents[2]))
+			sentences[sents[0]] = sents
+	return sentences
+
+def clearMem():
+	global sentences 
+	sentences = []
+	return sentences
+
+
+
+def getSentences(str):
+	
+	sents  = preprocessor.sentence_tokenizer(str)
+	sentNum = 0
+	sentence = [] 
+	global sentences
+	sentences = []
+	for sent in sents:
+		sentence.append(sentNum)
+		sentence.append(sent)
+		ID = preprocessor.LangDetect(sent)
+		if ID == "en":
+			sentence.append(getPOS(sent))
+			sentence.append(ID)
+			print(ID+" "+sent)
+		elif ID =="tl":
+			sentence.append(getFilPOS(preprocessor.tokenizer(sent)))
+			sentence.append(ID)
+			print(ID+" "+sent)
+		else :
+			sentence.append(getPOS(sent))
+			sentence.append(ID)
+			print(ID+" "+sent)
+		sentNum+=1
+		sentences.append(sentence)
+		sentence = []
+		
+	return sentences
+	#article.append(sentences)
+	
+	#article = [] #clears the article altogether
+	#for sent in article:
+	#	print(sent)
+	#	print("\n")
+	
+def getPOS(sent):
+	words = []
+	POS = preprocessor.posTagger(sent)
+	for item in POS:
+		word = (item[0],item[1])
+		words.append(word)
+	return words
+
+def getFilPOS(sent):
+	words = []
+	return preprocessor.filposTagger(sent)
