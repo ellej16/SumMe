2e311485c149e0bbeaec166aad87a788ca3a8754
ellej16 <pjinxed.aranzaellej@gmail.com>
2015-01-06 09:34:25 +0800

    Modify stuff

--- NLG/Sample.py
+++ NLG/Sample.py
@@ -15,6 +15,7 @@ def setUp():
 	np_the_curtain = NounPhrase(lex.getWord("curtain", "NOUN"), determiner=lex.getWord("the"))
 	pp_the_curtain = PrepositionalPhrase(lex.getWord("behind", "PREPOSITION"), [np_the_curtain])    
 	vp_kiss.add_prepositional_phrase(pp_the_curtain)
+	
 	np_man = vp_kiss.add_object(lex.getWord("man"))
 	np_man.add_determiner(lex.getWord("the"))    
 	s1.set_verb_tense("past")
