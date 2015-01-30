d7db02506f066321ba29be5abc7c325786c3fb84
giodude29 <evangelistarenzgio@gail.com>
2015-01-30 15:37:20 -0800

    updated sample3.py

--- NLG/Sample3.py
+++ NLG/Sample3.py
@@ -1,22 +1,22 @@
 from pynlg.realizer import Clause, ImperativeClause, PrepositionalPhrase, NounPhrase, SubjectPredicate, PredicateSubject, VerbPhrase
 from pynlg.lexicon import Word, XMLLexicon, Noun, Determiner, Adjective
 
-class Sample():
 
-        def setUp1():
+def setUp1():
 
-                #the girl is playing the guitar        
-                s1 = Clause()
-                
-                np_sub = s1.add_subject(lex.getWord("girl"))
-                np_sub.add_determiner(lex.getWord("the"))
-                vp_play = s1.add_verb(lex.getWord("play", "VERB"))
-        
-                np_guitar = vp_play.add_object(lex.getWord("guitar"))
-                np_guitar.add_determiner(lex.getWord("the"))
-        
-                s1.set_verb_tense("present_progressive")
+	lex = XMLLexicon()  
+	#the girl is playing the guitar        
+	s1 = Clause()
 
-                print(s1.realize())
+	np_sub = s1.add_subject(lex.getWord("girl"))
+	np_sub.add_determiner(lex.getWord("the"))
+	vp_play = s1.add_verb(lex.getWord("play", "VERB"))
 
-Sample.setUp1()
+	np_guitar = vp_play.add_object(lex.getWord("guitar"))
+	np_guitar.add_determiner(lex.getWord("the"))
+
+	s1.set_verb_tense("present_progressive")
+
+	print(s1.realize())
+
+setUp1()
